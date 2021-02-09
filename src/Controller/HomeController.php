ebd884e8397656dd2dcda5fb465f7b5cf73e1c2a
mclarck <contact.mclarck@gmail.com>
2021-02-09 00:58:16 -0300

    First Prod Build - Great Job!

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,9 +12,21 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/")
+     * @param Request $req
+     * @return Response
      */
-    public function index(Request $req)
+    public function index(Request $req): Response
     {
         return new Response("inmarketify");
+    }
+
+    /**
+     * @Route("/market/{_locale}/{company}/{path}",requirements={"path"=".+","_locale": "en|es|fr|pt"})
+     * @param Request $req
+     * @return Response
+     */
+    public function market(Request $req): Response
+    {
+        return $this->render('market/market.html.twig',[]);
     }
 }
